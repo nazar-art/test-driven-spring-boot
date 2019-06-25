e129c2798b4673fc6052a5275e24d7d4b85e6839
Nazar Lelyak <nazar.lelyak@chartermatch.com>
2019-06-25 17:44:56 +0300

    add some exclusion option to project

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test-driven-spring-boot
 
 Sample Spring Boot applications written in TDD style
+
--- src/main/java/com/xpinjection/springboot/LibraryApplication.java
+++ src/main/java/com/xpinjection/springboot/LibraryApplication.java
@@ -2,13 +2,19 @@ package com.xpinjection.springboot;
 
 import com.xpinjection.springboot.init.LibrarySettings;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JmxAutoConfiguration.class, WebSocketAutoConfiguration.class})
 @EnableConfigurationProperties(LibrarySettings.class)
 public class LibraryApplication extends SpringBootServletInitializer {
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return configureApplication(builder);
@@ -20,5 +26,13 @@ public class LibraryApplication extends SpringBootServletInitializer {
 
 	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
 		return builder.sources(LibraryApplication.class);
+	}
+
+	// deregister a filter
+	@Bean
+	public FilterRegistrationBean deRegisterHiddenHttpMethodFilter(HiddenHttpMethodFilter filter) {
+		FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+		bean.setEnabled(false);
+		return bean;
 	}
 }
